a2e5e1b55d341b2f15eeb2cbd7d53fea21327fea
jacekkowalczyk <s29829@pjwstk.edu.pl>
2024-05-20 18:45:29 +0200

    v001

--- src/Student.java
+++ src/Student.java
@@ -1,5 +1,20 @@
+import java.util.Date;
+
 public class Student {
 
+    private String name, surename, email, address, phoneNumber;
+    private int year,mont,date;
+    private Date dateOfBirth;
+
+
+    public Student(String name, String surename, String email, String address, String phoneNumber, Date dateOfBirth) {
+        this.name = name;
+        this.surename = surename;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.dateOfBirth = dateOfBirth;
+    }
 
     public void enrollStudent(StudyProgramme it) {
     }
--- src/StudyProgramme.java
+++ src/StudyProgramme.java
@@ -1,2 +1,39 @@
 public class StudyProgramme {
+
+    private String dd, aa;
+    private int bb, aa1;
+
+    //         StudyProgramme it = new StudyProgramme("IT", "AAA", 7, 5);
+
+
+    public StudyProgramme(String dd, String aa, int bb, int aa1) {
+        this.dd = dd;
+        this.aa = aa;
+        this.bb = bb;
+        this.aa1 = aa1;
+    }
+
+    public String getDd() {
+        return dd;
+    }
+
+    public void setDd(String dd) {
+        this.dd = dd;
+    }
+
+    public String getAa() {
+        return aa;
+    }
+
+    public void setAa(String aa) {
+        this.aa = aa;
+    }
+
+    public int getBb() {
+        return bb;
+    }
+
+    public void setBb(int bb) {
+        this.bb = bb;
+    }
 }
